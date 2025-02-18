52dae4a038494e9758553978c3f82c9d17bc48b2
Jonathan Olsson <jo3405ol-s@saturnus-3.e.lth.lu.se>
2025-02-18 13:58:30 +0100

    started

---
+++ lab/lab3/test_TagRemover.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "TagRemover.h"
+
+using namespace std;
+
+string test_tagRemover(string str) {
+    TagRemover tr(str);
+    return tr.print();
+}
+
+int main() {
+    string str = "test";
+    string result = test_TagRemover(str);
+    if (result == str) {
+        cout << "Success" << "\n";
+    } else {
+        cout << "Failure" << "\n";
+    }
+}
