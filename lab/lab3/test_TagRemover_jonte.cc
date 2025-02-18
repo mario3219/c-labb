47f11656d33fd6380bf0b3519cc49aeeed8e87cd
mario3219 <jonathan.olsson8@gmail.com>
2025-02-18 14:47:32 +0100

    tagremover klar

--- lab/lab3/test_TagRemover_jonte.cc
+++ lab/lab3/test_TagRemover_jonte.cc
@@ -3,34 +3,39 @@
 
 using namespace std;
 
-bool test_tagRemover(string str) {
+bool test_tagRemover() {
     TagRemover tr();
     string test;
     string goal;
     string result;
 
+    cout << "test_tagRemover - "
+
     test = "This is a line without a tag";
     tr.setString(test);
     result = tr.removeTags();
-    if (result != test) {return false;}
+    if (result != test) {
+        cout << "Failure at: This is a line without a tag" << "\n";
+    return false;
+    }
 
     test = "This is <tag tag> a line with one tag";
     goal = "This is a line with one tag";
     tr.setString(test);
     result = tr.removeTags();
-    if (result != goal) {return false;}
+    if (result != goal) {
+        cout << "Failure at: This is <tag tag> a line with one tag" << "\n";
+    return false;
+    }
     
+    cout << "Pass" << "\n";
+
     return true;
 }
 
 int main() {
-    string str = "test";
-    string result = test_TagRemover(str);
-    if (result == str) {
-        cout << "Success" << "\n";
-    } else {
-        cout << "Failure" << "\n";
-    }
+    test_TagRemover(str);
+    return 0;
 }
 
 string test_specCharRemover(string str) {
