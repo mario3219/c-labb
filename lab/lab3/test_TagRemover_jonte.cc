8e60fc4d7ea0107530e08f2977e01269917e7211
Joel-hogberg <joel.erik.hogberg@gmail.com>
2025-02-18 15:52:59 +0100

    fixade test_tagremover_jonte

--- lab/lab3/test_TagRemover_jonte.cc
+++ lab/lab3/test_TagRemover_jonte.cc
@@ -8,11 +8,12 @@ bool test_tagRemover() {
     string test;
     string goal;
     string result;
+    istringstream testStream(test);
 
     cout << "test_tagRemover - ";
 
     test = "This is a line without a tag";
-    TagRemover tr(istringstream istream(test));
+    TagRemover tr(testStream);
     result = tr.removeTags();
     if (result != test) {
         cout << "Failure at: This is a line without a tag" << "\n";
@@ -20,7 +21,7 @@ bool test_tagRemover() {
     }
 
     test = "This is <tag tag> a line with one tag";
-    TagRemover tr(istringstream istream(test));
+    TagRemover tr(testStream);
     goal = "This is a line with one tag";
     result = tr.removeTags();
     if (result != goal) {
@@ -33,15 +34,15 @@ bool test_tagRemover() {
     return true;
 }
 
-int main() {
-    test_TagRemover();
-    return 0;
-}
-
 string test_specCharRemover(string str) {
     TagRemover tr(str);
     tr.removeSpecChars();
     return tr.print();
+}
+
+int main() {
+    test_tagRemover();
+    return 0;
 }
     
 
