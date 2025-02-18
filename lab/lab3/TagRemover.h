effb2fa026c5b93aa437fe77dfec479e71be6d4c
mario3219 <jonathan.olsson8@gmail.com>
2025-02-18 15:36:02 +0100

    string to stream tagRemover in test

--- lab/lab3/TagRemover.h
+++ lab/lab3/TagRemover.h
@@ -5,17 +5,11 @@ using namespace std;
 
 class TagRemover {
     public:
-<<<<<<< HEAD
-        TagRemover();
-        string specCharRemover(string& str);
-
-=======
         TagRemover(std::istream& input);
         string removeTags();
         string removeSpecChars();
         string print();
         string setString(string str);
->>>>>>> d712eb03f1280bad623dd3c70bd00d79d29c2b35
     private:
         string text;
         string result;
