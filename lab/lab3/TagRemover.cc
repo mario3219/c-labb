9a62031777facd011a67abd750c42edcc5866081
mario3219 <jonathan.olsson8@gmail.com>
2025-02-18 16:38:15 +0100

    fixade make filen och removeTags

--- lab/lab3/TagRemover.cc
+++ lab/lab3/TagRemover.cc
@@ -2,47 +2,50 @@
 #include <iostream>
 #include <algorithm>
 #include <sstream>
-#include <vector>
+#include <map>
 #include "TagRemover.h"
 
 using namespace std;
 
-TagRemover::TagRemover(std::istream& input) : text(""), result("") {
+TagRemover::TagRemover(std::istream& input) : text("") {
     string str;
     while (getline(input, str)) {
         text += str;
     }
 }
 
-string TagRemover::specCharRemover(string& str) {
+string TagRemover::specCharRemover() {
+    string temp;
+    map<string,string> mp;
+    mp["&lt"] = 
+    mp["&gt"] =
+    mp["&nbsp"] =
     string toReplace = "test";
     string replaceWith = "new";
     size_t pos;
     while (pos != string::npos) {
-        pos = str.find(toReplace);
-        str.replace(pos, toReplace.length(), replaceWith);
+        pos = text.find(toReplace);
+        text.replace(pos, toReplace.length(), replaceWith);
     }
-    return str;
+    return text;
 };
 
 string TagRemover::getText() {
     return text;
 }
 
-string TagRemover::getResult() {
-    return result;
-}
-
 string TagRemover::removeTags() {
     bool tag = false;
+    string temp;
     for (char c : text) {
         if (c == '<') {
             tag = true;
         } else if (c == '>') {
             tag = false;
         } else if (!tag) {
-            result += c;
+            temp += c;
         }
     }
-    return result;
+    text = temp;
+    return text;
 }
--- lab/lab3/TagRemover.h
+++ lab/lab3/TagRemover.h
@@ -11,14 +11,8 @@ class TagRemover {
         string print();
         string setString(string str);
         string getText();
-<<<<<<< Updated upstream
-        string getResult();
-=======
-        string specCharRemover(string& str);
->>>>>>> Stashed changes
     private:
         string text;
-        string result;
 };
 
 #endif
