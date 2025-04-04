edf9a553d98c884749b2d5368d0b11e3126bf03e
Joel-hogberg <joel.erik.hogberg@gmail.com>
2025-04-04 12:45:39 +0200

    Startad MemoryDatabase.cc

---
+++ project/news-project/src/database/MemoryDatabase.cc
@@ -0,0 +1,37 @@
+#include "MemoryDatabase.h"
+
+MemoryDatabase::MemoryDatabase() {}
+
+std::vector<Newsgroup> MemoryDatabase::listNewsgroups() const
+{
+    std::vector<Newsgroup> result;
+    for (const auto &pair : newsgroups)
+    {
+        result.push_back(pair.second);
+    }
+    return result;
+}
+
+bool MemoryDatabase::createNewsgroup(const std::string &name)
+{
+}
+
+bool MemoryDatabase::deleteNewsgroup(std::string newsgroup_name)
+{
+}
+
+std::vector<Article> MemoryDatabase::listArticles(std::string newsgroup_name) const
+{
+}
+
+bool MemoryDatabase::createArticle(std::string newsgroup_name, const std::string &title, const std::string &author, const std::string &content)
+{
+}
+
+bool MemoryDatabase::deleteArticle(std::string newsgroup_name, int articleId)
+{
+}
+
+Article MemoryDatabase::getArticle(std::string newsgroup_name, int articleId) const
+{
+}
