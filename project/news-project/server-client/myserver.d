6cfb2b4685f1f29e0bbe07242e26270218a729a1
mario3219 <jonathan.olsson8@gmail.com>
2025-04-09 17:37:39 +0200

    definierat functioner för messagehandler och commandhandler, implementerat en någorlunda ui i client

--- project/news-project/server-client/myserver.d
+++
@@ -1,7 +0,0 @@
-myserver.o: myserver.cc ../include/connection.h \
- ../include/connectionclosedexception.h ../include/server.h \
- ../include/connection.h
-../include/connection.h:
-../include/connectionclosedexception.h:
-../include/server.h:
-../include/connection.h:
