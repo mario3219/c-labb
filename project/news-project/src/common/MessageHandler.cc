cb7097f166f5fedb3bfed7b67b2244cd5a10bfd6
haraldolsson <90654023+haraldolsson@users.noreply.github.com>
2025-04-16 13:23:02 +0200

    Add files via upload

--- project/news-project/src/common/MessageHandler.cc
+++ project/news-project/src/common/MessageHandler.cc
@@ -1,22 +1,89 @@
 #include "MessageHandler.h"
+#include <memory>
 
+using std::string;
 /*Constructors and destructors*/
-MessageHandler::MessageHandler(const Connection& conn) : conn(conn) {}
-
-/*Sending methods*/
-void MessageHandler::sendCode(int code) {};
-void MessageHandler::sendInt(int value) {};
-void MessageHandler::sendString(string str) {};
-void MessageHandler::sendStringParameter(string param) {};
-void MessageHandler::sendIntParameter(int param) {};
-
-/*Receiving methods*/
-int MessageHandler::recCode() {return 0;};
-int MessageHandler::recInt() {return 0;};
-string MessageHandler::recString() {return ""};
-string MessageHandler::recStringParameter() {return ""};
-int MessageHandler::recIntParameter() {return 0};
+MessageHandler::MessageHandler(const std::shared_ptr<Connection>& con) : conn(con) {}
 
 /*Private methods*/
-void MessageHandler::sendByte(int code) {};
-void MessageHandler::recByte() {};
+void MessageHandler::sendByte(int code) {
+    conn->write(static_cast<char>(code));
+}
+int MessageHandler::recByte() {
+    int code = static_cast<int>(conn->read());
+    return code;
+}
+
+
+/*Sending methods*/
+
+void MessageHandler::sendCode(Protocol code) {
+    sendByte(static_cast<int>(code));
+}
+
+void MessageHandler::sendInt(int value) {
+    sendByte((value >> 24) & 0xFF);
+    
+    sendByte((value >> 16) & 0xFF);
+    
+    sendByte((value >> 8) & 0xFF);
+    
+    sendByte(value & 0xFF);
+    
+}
+
+//void MessageHandler::sendString(string str) {};
+
+void MessageHandler::sendStringParameter(string param) {
+    sendCode(Protocol::PAR_STRING);
+    sendInt(static_cast<int>(param.length()));
+    for (size_t i = 0; i < param.length(); i++) {
+        sendByte(param[i]);
+    }
+};
+
+void MessageHandler::sendIntParameter(int param) {
+    sendCode(Protocol::PAR_NUM);
+    sendInt(param);
+}
+
+/*Receiving methods*/
+int MessageHandler::recCode() {
+    int code = recByte();
+    return code;
+}
+
+int MessageHandler::recInt() {
+    int b1 = recByte();
+
+    int b2 = recByte();
+
+    int b3 = recByte();
+
+    int b4 = recByte();
+
+    return b1 << 24 | b2 << 16 | b3 << 8 | b4;
+}
+
+//string MessageHandler::recString() {return ""};
+
+
+string MessageHandler::recStringParameter() {
+    //int code = recCode();
+
+    int n = recInt();
+
+    string result;
+
+    for (int i = 0; i < n; i++) {
+        char ch = static_cast<char>(conn->read());
+        result += ch;
+    }
+    return result;
+}
+
+int MessageHandler::recIntParameter() {
+    //int code = recCode();
+    return recInt();
+}
+
