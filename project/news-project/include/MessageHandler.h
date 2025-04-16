be7bb762b27d44ada4be6dce4d5d95ca635d3406
haraldolsson <90654023+haraldolsson@users.noreply.github.com>
2025-04-16 13:27:32 +0200

    Add files via upload

--- project/news-project/include/MessageHandler.h
+++ project/news-project/include/MessageHandler.h
@@ -2,35 +2,37 @@
 #define MESSAGEHANDLER_H
 
 #include "connection.h"
+#include "protocol.h"
 #include <string>
-
+#include <memory>
 using std::string;
 
 class MessageHandler {
     public:
         /*Constructors and destructors*/
-        MessageHandler(const Connection& conn);
+        MessageHandler(const std::shared_ptr<Connection>& con);
+        //MessageHandler(const Connection& con);
         ~MessageHandler() = default;
 
         /*Sending methods*/
-        void sendCode(int code);
+        void sendCode(Protocol code);
         void sendInt(int value);                    
-        void sendString(string str);
+        //void sendString(string str);
         void sendStringParameter(string param);
         void sendIntParameter(int param);
         
         /*Receiving methods*/
         int recCode();
         int recInt();
-        string recString();
+        //string recString();
         string recStringParameter();
         int recIntParameter();
 
     private:
         void sendByte(int code);
-        void recByte();
-
-        Connection& conn;
+        int recByte();
+        std::shared_ptr<Connection> conn;
+        //Connection& conn;
 };
 
 #endif
