aaff8e5751c6e6d4eb89a40412f1c44e277dbda0
Joel-hogberg <joel.erik.hogberg@gmail.com>
2025-04-16 15:49:29 +0200

    lite fix i myserver (OBS: TEST)

--- project/news-project/server-client/myserver.cc
+++ project/news-project/server-client/myserver.cc
@@ -3,6 +3,9 @@
 #include "connectionclosedexception.h"
 #include "server.h"
 #include "MessageHandler.h"
+#include "protocol.h"
+// Update the include path to the correct relative or absolute path
+#include "../include/MemoryDatabase.h"
 
 #include <cstdlib>
 #include <iostream>
@@ -10,39 +13,102 @@
 #include <stdexcept>
 #include <string>
 
-using std::string;
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
+using std::string;
 
 /* --------------------------------------Server methods--------------------------------------------*/
 
-int readNumber(const std::shared_ptr<Connection>& conn) {return 0;}
+int readNumber(const std::shared_ptr<Connection> &conn) { return 0; }
 
-void writeString(const std::shared_ptr<Connection>& conn, const string& s) {}
+void writeString(const std::shared_ptr<Connection> &conn, const string &s) {}
+
+void listNewsgroups(const std::shared_ptr<Connection> &conn, const string &s, MemoryDatabase db)
+{
+        auto newsgroups = db.listNewsgroups();
+        for (const auto &newsgroup : newsgroups)
+        {
+                cout << "Newsgroup: " << newsgroup.name << endl;
+        }
+}
+
+void createNewsgroup(const std::shared_ptr<Connection> &conn, const string &s, MemoryDatabase db, string name)
+{
+        if (db.createNewsgroup(name))
+        {
+                cout << "Newsgroup created: " << name << endl;
+        }
+        else
+        {
+                cout << "Failed to create newsgroup: " << name << endl;
+        }
+}
+
+void deleteNewsgroup(const std::shared_ptr<Connection> &conn, const string &s, MemoryDatabase db, string name)
+{
+        if (db.deleteNewsgroup(name))
+        {
+                cout << "Newsgroup deleted: " << name << endl;
+        }
+        else
+        {
+                cout << "Failed to delete newsgroup: " << name << endl;
+        }
+}
+
+void listArticles(const std::shared_ptr<Connection> &conn, const string &s, MemoryDatabase db, string newsgroup_name)
+{
+        auto articles = db.listArticles(newsgroup_name);
+        for (const auto &article : articles)
+        {
+                cout << "Article ID: " << article.id << ", Title: " << article.title << endl;
+        }
+}
 
 /* --------------------------------------Server runtime--------------------------------------------*/
 
-void serve_one(Server& server)
+void serve_one(Server &server, MemoryDatabase db)
 {
         auto conn = server.waitForActivity();
-        if (conn != nullptr) {
-                try {
+        if (conn != nullptr)
+        {
+                try
+                {
+                        MessageHandler msgh(*conn);
 
-                        /* Server commands
-                        Suggested format: 
-                        command = conn.read()
-                        if command = LIST_GROUPS
-                                Commandhander.writeList(list)
-                        if command = SIZE_LIST
-                                commandhandler.writesize(size)
-                        ...
-                        */
-                } catch (ConnectionClosedException&) {
+                        Protocol command = static_cast<Protocol>(msgh.recCode());
+
+                        switch (command)
+                        {
+                        case Protocol::COM_LIST_NG:
+                                listNewsgroups(conn, "list", db);
+                                break;
+                        case Protocol::COM_CREATE_NG:
+                        {
+                                string name = msgh.recStringParameter();
+                                createNewsgroup(conn, "create", db, name);
+                                msgh.sendCode(Protocol::ANS_CREATE_NG);
+                        }
+                                /* Server commands
+                                Suggested format:
+                                command = conn.read()
+                                if command = LIST_GROUPS
+                                        Commandhander.writeList(list)
+                                if command = SIZE_LIST
+                                        commandhandler.writesize(size)
+                                ...
+                                */
+                        }
+                } // Close the switch statement
+                catch (ConnectionClosedException &)
+                {
                         server.deregisterConnection(conn);
                         cout << "Client closed connection" << endl;
                 }
-        } else {
+        }
+        else
+        {
                 conn = std::make_shared<Connection>();
                 server.registerConnection(conn);
                 cout << "New client connects" << endl;
@@ -51,35 +117,43 @@ void serve_one(Server& server)
 
 /* --------------------------------------Server initializers - don't touch--------------------------------------------*/
 
-Server init(int argc, char* argv[])
+Server init(int argc, char *argv[])
 {
-        if (argc != 2) {
+        if (argc != 2)
+        {
                 cerr << "Usage: myserver port-number" << endl;
+
                 exit(1);
         }
 
         int port = -1;
-        try {
+        try
+        {
                 port = std::stoi(argv[1]);
-        } catch (std::exception& e) {
+        }
+        catch (std::exception &e)
+        {
                 cerr << "Wrong format for port number. " << e.what() << endl;
                 exit(2);
         }
 
         Server server(port);
-        if (!server.isReady()) {
+        if (!server.isReady())
+        {
                 cerr << "Server initialization error." << endl;
                 exit(3);
         }
         return server;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+        auto db = MemoryDatabase();
         auto server = init(argc, argv);
 
-        while (true) {
-            serve_one(server);
+        while (true)
+        {
+                serve_one(server, db);
         }
         return 0;
 }
